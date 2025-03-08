2c9a5694ed176e620d9ac056b14c09c9333f771e
ragayathri97 <ragayathri97@gmail.com>
2025-03-09 00:25:02 +0530

    correction

--- src/components/TopContent/TopContent.jsx
+++ src/components/TopContent/TopContent.jsx
@@ -4,7 +4,6 @@ import "./topContent.css";
 import gayuimg from "../../assets/images/Gayuimg.jpeg";
 import resumePdf from "../../assets/resume/GayathriResume.pdf";
 
-
 const TopContent = () => {
   const handleDownload = () => {
     window.open(resumePdf, '_blank');
